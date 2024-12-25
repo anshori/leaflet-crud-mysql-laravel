b29f050efe5ca1c6bc1dab44577cf0b367e187d6
anshori <m.anshory@gmail.com>
2024-12-25 10:45:50 +0700

    Add link to GeoJSON API at navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,6 +11,16 @@
           <a class="nav-link" href="{{ route('/') }}"><i class="bi bi-globe-asia-australia"></i> Home</a>
         </li>
         @endif
+				<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-stack"></i> GeoJSON API
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{ route('geojson.points') }}" target="_blank"><i class="bi bi-geo-alt-fill"></i> Points</a></li>
+            <li><a class="dropdown-item" href="{{ route('geojson.polylines') }}" target="_blank"><i class="bi bi-dash-lg"></i> Polylines</a></li>
+            <li><a class="dropdown-item" href="{{ route('geojson.polygons') }}" target="_blank"><i class="bi bi-pentagon"></i> Polygons</a></li>
+          </ul>
+        </li>
         <li class="nav-item">
           <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#infoModal"><i class="bi bi-info-circle-fill"></i> Info</a>
         </li>
